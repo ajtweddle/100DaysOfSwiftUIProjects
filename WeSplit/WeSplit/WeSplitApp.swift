832b4f0ef487cc0f3f1c8a3990825eab762e33a8
Allan Tweddle <allan@allantweddle.com>
2023-03-27 20:09:01 +0100

    Project 1: WeSplit, before challenges

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Allan Tweddle on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
