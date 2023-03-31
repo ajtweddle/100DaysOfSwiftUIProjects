e2ff44827bfbeea9564b5e4f74cf47a94a11fe8b
Allan Tweddle <allan@allantweddle.com>
2023-03-31 16:12:19 +0100

    Project 2: GuessTheFlag, challenge 1, added and displayed player's score

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var userScore = 0
 
     var body: some View {
         ZStack {
@@ -56,7 +57,7 @@ struct ContentView: View {
 
 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -67,13 +68,14 @@ Spacer()
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(userScore)")
         }
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
             scoreTitle = "Wrong"
         }
