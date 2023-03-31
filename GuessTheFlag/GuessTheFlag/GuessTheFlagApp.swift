10907478e7212c5e6e9f086601baf58b1161f2dc
Allan Tweddle <allan@allantweddle.com>
2023-03-31 15:39:37 +0100

    Project2: GuessTheFlag, before challenges

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Allan Tweddle on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
