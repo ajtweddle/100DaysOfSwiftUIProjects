36cd298e2451f8b15b86cdbefe26534af4820800
Allan Tweddle <allan@allantweddle.com>
2023-04-14 16:24:19 +0100

    Project 4: BetterRest, before challenges

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Allan Tweddle on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
