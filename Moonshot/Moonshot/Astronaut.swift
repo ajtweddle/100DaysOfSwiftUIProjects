7065f745bbf1c9a03ebccf09f629585902a1680e
Allan Tweddle <allan@allantweddle.com>
2023-04-24 15:09:01 +0100

    Project 8: Moonshot, before challenges.

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Allan Tweddle on 21/04/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
