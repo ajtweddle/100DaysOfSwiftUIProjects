91210805ec8ea45b5b780f209496c9d877c5c11e
Allan Tweddle <allan@allantweddle.com>
2023-04-28 10:08:00 +0100

    Project 10: CupcakeCorner, before challenges.

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Allan Tweddle on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
