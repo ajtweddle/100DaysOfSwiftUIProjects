3cfea7f5ea79176faeb0bb6d8416315d4380484a
Allan Tweddle <allan@allantweddle.com>
2023-05-03 16:10:17 +0100

    Project 11: Bookworm, added content and detail view plus basic sort descriptors for @FetchRequest

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
 
     @State private var showingAddScreen = false
 
     var body: some View {
         NavigationView {
-           Text("Count: \(books.count)")
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
                .navigationTitle("Bookworm")
                .toolbar {
                    ToolbarItem(placement: .navigationBarTrailing) {
