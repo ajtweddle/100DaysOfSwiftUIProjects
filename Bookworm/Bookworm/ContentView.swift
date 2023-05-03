c453a8128da8bcb0bff8387a455de6afd9365be3
Allan Tweddle <allan@allantweddle.com>
2023-05-03 10:06:56 +0100

    Project 11: Bookworm, creating books with CoreData.

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Allan Tweddle on 02/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
+
+    var body: some View {
+        NavigationView {
+           Text("Count: \(books.count)")
+               .navigationTitle("Bookworm")
+               .toolbar {
+                   ToolbarItem(placement: .navigationBarTrailing) {
+                       Button {
+                           showingAddScreen.toggle()
+                       } label: {
+                           Label("Add Book", systemImage: "plus")
+                       }
+                   }
+               }
+               .sheet(isPresented: $showingAddScreen) {
+                   AddBookView()
+               }
+       }
+        .padding()
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
