b0da69c488fd6a398d9589af2886c2566bf86ecd
Allan Tweddle <allan@allantweddle.com>
2023-05-09 14:49:20 +0100

    Project 13: Instafilter, before challenges.

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Allan Tweddle on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
