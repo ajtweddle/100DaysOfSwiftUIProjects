330e71a5db289c1b8895ca062f36bf8181b244d9
Allan Tweddle <allan@allantweddle.com>
2023-05-12 16:09:12 +0100

    Project 15: CupcakeCorner, hid image loading to prevent VoiceOver chatter.

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -23,6 +23,8 @@ struct CheckoutView: View {
                     ProgressView()
                 }
                 .frame(height: 233)
+                // Hide image loading to stop VoiceOver chatter
+                .accessibilityHidden(true)
 
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
