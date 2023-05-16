09979d9f0f5bcf18fce72be5b52bb6b6c6997fe4
Allan Tweddle <allan@allantweddle.com>
2023-05-16 17:22:15 +0100

    Project 16: HotProspects, before challenges.

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Allan Tweddle on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
