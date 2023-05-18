079a452fe98717f930e63cff9deaad9dc66e815b
Allan Tweddle <allan@allantweddle.com>
2023-05-18 10:30:25 +0100

    Project 17: Flashzilla, before challenges.

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Allan Tweddle on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
