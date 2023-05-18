2e462388a98fb1e9e6c40eefbeb3aa5149f89002
Allan Tweddle <allan@allantweddle.com>
2023-05-18 20:52:47 +0100

    Project 19: SnowSeeker, before challenges.

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Allan Tweddle on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
